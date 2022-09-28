608818cb8f2e57a3ce763257ce4b0696694ea2aa
avukadin <avukadin@uwaterloo.ca>
2022-09-28 06:41:27 -0400

    progress

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/avukadin/goapi/pkg/test"
+import (
+	"net/http" 
+	"github.com/go-chi/chi"
+	"github.com/avukadin/goapi/pkg/mongocon"
+)
+
+
+func getArticle(w http.ResponseWriter, r *http.Request){
+	var out = []byte("Hello from getArticle");
+	w.Write(out);
+}
 
 func main(){
-	
+	mongocon.getMongoConnection()
+	r := chi.NewRouter();
+	r.Get("/getArticle", getArticle)
+
+	// http.ListenAndServe(":8080", r)
 }
